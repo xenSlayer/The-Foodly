dd66f62bae390150238e6e865f27c2b36d6a7816
aistha11 <bijayshrestha0908@gmail.com>
2020-07-31 14:07:47 +0545

    ordered items on order details

--- foodly_dashboard/lib/src/dashboard/widget/orderDetails.dart
+++ foodly_dashboard/lib/src/dashboard/widget/orderDetails.dart
@@ -5,6 +5,7 @@ import 'package:foodly_dashboard/constants/icons.dart';
 import 'package:foodly_dashboard/constants/strings.dart';
 import 'package:foodly_dashboard/constants/styles.dart';
 import 'package:foodly_dashboard/models/order.dart';
+import 'package:foodly_dashboard/src/dashboard/widget/dashOrderItemTile.dart';
 import 'package:foodly_dashboard/utilities/utils.dart';
 
 class OrderDetails extends StatelessWidget {
@@ -49,50 +50,70 @@ class OrderDetails extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverFillRemaining(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  //Date, id
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 25.0, top: 5.0, bottom: 5),
-                    child: Text(
-                      "${Utils.getFirstInitials(order.orderDate.toString())} #${order.id}",
-                      style: TextStyle(fontSize: 16.0, color: black),
-                    ),
-                  ),
-                  //emailAdress
-                  dataDetailView("Email Address:", "${order.emailAddress}"),
-                  //order Status
-                  dataDetailView("Order Status:", "${order.orderState}"),
-                  //order Items
-                  //order total
-                  dataDetailView(
-                      "Order Total:", "$currency ${order.totalAmount}"),
-                  //contact number
-                  dataDetailView("Contact Number:", "${order.mobileNumber}"),
-                  //billing address
-                  dataDetailView(
-                      "Billing Address:", "${order.deliveryAddress}"),
-                  dataDetailView(
-                      "Device Location:", "${order.deviceLocation}"),
-                  //coupon
-                  order.coupon != "" ? dataDetailView("Coupon:", "${order.coupon}"): Container(),
-                  //special note
-                  order.specialNote != "" ?dataDetailView("Special Note:", "${order.specialNote}"):Container(),
-                  //items
-                  dataDetailView(
-                      "Order Items:",""),
-                  
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Date, id
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0, top: 5.0, bottom: 5),
+              child: Text(
+                "${Utils.getFirstInitials(order.orderDate.toString())} #${order.id}",
+                style: TextStyle(fontSize: 16.0, color: black),
               ),
             ),
-          ),
-        ],
+            //emailAdress
+            dataDetailView("Email Address:", "${order.emailAddress}"),
+            //order Status
+            dataDetailView("Order Status:", "${order.orderState}"),
+            //order Items
+            //order total
+            dataDetailView("Order Total:", "$currency ${order.totalAmount}"),
+            //contact number
+            dataDetailView("Contact Number:", "${order.mobileNumber}"),
+            //billing address
+            dataDetailView("Billing Address:", "${order.deliveryAddress}"),
+            dataDetailView("Device Location:", "${order.deviceLocation}"),
+            //coupon
+            order.coupon != ""
+                ? dataDetailView("Coupon:", "${order.coupon}")
+                : Container(),
+            //special note
+            order.specialNote != ""
+                ? dataDetailView("Special Note:", "${order.specialNote}")
+                : Container(),
+            //items
+            Padding(
+                padding:
+                    const EdgeInsets.only(left: 25.0, top: 15.0, bottom: 5),
+                child: Text(
+                  "Order Items:",
+                  style: TextStyle(fontSize: 20.0, color: mainCol),
+                ),
+              ),
+            // Flexible(
+            //   child: Container(
+            //     height: Utils.getHeightByPercentage(context, 50),
+            //     child: ListView.builder(
+            //       itemCount: order.orderedItems.length,
+            //       itemBuilder: (_, i) {
+            //         OrderedItem orderedItem = order.orderedItems[i];
+            //         return DashOrderItemTile(orderedItem);
+            //       },
+            //     ),
+            //   ),
+            // ),
+            Container(
+              padding: EdgeInsets.only(left:25, top: 5),
+              child: Wrap(
+                alignment: WrapAlignment.spaceEvenly,
+                children: order.orderedItems.map((orderedItem) {
+                  return DashOrderItemTile(orderedItem);
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
